a473dacd82db87dcc3c99bf6b2da92c1c9db71f3
stites <sam@stites.io>
2015-08-26 14:58:12 -0700

    init- exercises for 3

---
+++ src/ch03/exercises.hs
@@ -0,0 +1,23 @@
+-- 8. Using the binary tree type that we defined earlier in this chapter,
+-- write a function that will determine the height of the tree. The height is
+-- the largest number of hops from the root to an Empty. For example, the tree
+-- Empty has height zero; Node "x" Empty Empty has height one; Node "x" Empty
+-- (Node "y" Empty Empty) has height two; and so on.
+--
+-- 9. Consider three two-dimensional points a, b, and c. If we look at the
+-- angle formed by the line segment from a to b and the line segment from b to
+-- c, it either turns left, turns right, or forms a straight line. Define a
+-- Direction data type that lets you represent these possibilities.
+--
+-- 10. Write a function that calculates the turn made by three 2D points and
+-- returns a Direction. 59 comments
+--
+-- 11. Define a function that takes a list of 2D points and computes the
+-- direction of each successive triple. Given a list of points [a,b,c,d,e], it
+-- should begin by computing the turn made by [a,b,c], then the turn made by
+-- [b,c,d], then [c,d,e]. Your function should return a list of Direction.
+--
+-- 12. Using the code from the preceding three exercises, implement Graham's
+-- scan algorithm for the convex hull of a set of 2D points. You can find good
+-- description of what a convex hull. is, and how the Graham scan algorithm
+-- should work, on Wikipedia.
