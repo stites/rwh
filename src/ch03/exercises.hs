f593aaafdba724ad1b39e13112318d3dbb79a2d4
stites <sam@stites.io>
2015-08-28 15:52:17 -0700

    add - Graham's scan algorithm

--- src/ch03/exercises.hs
+++ src/ch03/exercises.hs
@@ -16,16 +16,20 @@ treeHeight (Node _ a b) = 1 + max ( treeHeight a ) ( treeHeight b )
 -- angle formed by the line segment from a to b and the line segment from b to
 -- c, it either turns left, turns right, or forms a straight line. Define a
 -- Direction data type that lets you represent these possibilities.
---
--- 10. Write a function that calculates the turn made by three 2D points and
--- returns a Direction. 59 comments
---
--- 11. Define a function that takes a list of 2D points and computes the
--- direction of each successive triple. Given a list of points [a,b,c,d,e], it
--- should begin by computing the turn made by [a,b,c], then the turn made by
--- [b,c,d], then [c,d,e]. Your function should return a list of Direction.
---
+
+data Point = Point Integer Integer deriving (Show)
+data Direction = DLeft | DStraight | DRight deriving (Eq, Show)
+
 -- 12. Using the code from the preceding three exercises, implement Graham's
 -- scan algorithm for the convex hull of a set of 2D points. You can find good
 -- description of what a convex hull. is, and how the Graham scan algorithm
 -- should work, on Wikipedia.
+
+direction :: Point -> Point -> Point -> Direction
+direction p1 p2 p3
+        | cross p1 p2 p3 < 0 = DRight
+        | cross p1 p2 p3 > 0 = DLeft
+        | otherwise          = DStraight
+        where cross (Point x1 y1) (Point x2 y2) (Point x3 y3) =
+                    (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
+
