59af50af89d9b042f4b58d658175147cb119f76d
stites <sam@stites.io>
2015-08-31 00:28:40 -0700

    update - ch4 code

--- src/ch04/InteractWith.hs
+++ src/ch04/InteractWith.hs
@@ -22,7 +22,19 @@ main = mainWith myFunction
           case args of
             [input,output] -> interactWith function input output
             _ -> putStrLn "error: exactly two arguments needed"
+        myFunction = fixLines
 
-        -- replace "id" with the name of our function below
-        myFunction = id
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminator cs
+    in  pre : case suf of
+                ('\r':'\n':rest) -> splitLines rest
+                ('\r':rest)      -> splitLines rest
+                ('\n':rest)      -> splitLines rest
+                _                -> []
+
+isLineTerminator c = c == '\r' || c == '\n'
+
+fixLines :: String -> String
+fixLines input = unlines (splitLines input)
 
