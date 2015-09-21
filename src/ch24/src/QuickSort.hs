37971fab494eb23d4d6a91762bcfddd4068505cd
stites <sam@stites.io>
2015-09-21 10:27:48 -0700

    add - benchmarking sorts

--- src/ch24/src/QuickSort.hs
+++ src/ch24/src/QuickSort.hs
@@ -1,6 +1,7 @@
-module Sorting where
+module QuickSort where
 
 import Control.Parallel (par, pseq)
+import Control.DeepSeq
 
 sort :: (Ord a) => [a] -> [a]
 sort (x:xs) = lesser ++ x:greater
@@ -8,15 +9,13 @@ sort (x:xs) = lesser ++ x:greater
           greater = sort [y | y <- xs, y >= x]
 sort _ = []
 
-parSort :: (Ord a) => [a] -> [a]
+parSort :: (NFData a, Ord a) => [a] -> [a]
 -- force evaluates the entire spine of a list before we give back a constructor
 -- kind of like taking WHNF -> NF. or deepseq
-parSort (x:xs)    = force greater `par` (force lesser `pseq`
-                                         (lesser ++ x:greater))
-    where lesser  = parSort [y | y <- xs, y <  x]
+parSort (x:xs)    = force greater `par` (force lesser `pseq` (lesser ++ x:greater))
+    where lesser  = parSort [y | y <- xs, y < x]
           greater = parSort [y | y <- xs, y >= x]
 parSort _         = []
-
 
 -- Instead of force lesser and force greater, here we evaluate lesser and greater:
 sillySort (x:xs) = greater `par` (lesser `pseq`
@@ -31,3 +30,21 @@ sillySort _        = []
 -- Every other element of each list remains unevaluated.
 -- In other words, we do almost no useful work in parallel: our sillySort is
 -- nearly completely sequential.
+
+seqSort :: (Ord a) => [a] -> [a]
+seqSort (x:xs) = lesser `pseq` (greater `pseq`
+                                (lesser ++ x:greater))
+    where lesser  = seqSort [y | y <- xs, y <  x]
+          greater = seqSort [y | y <- xs, y >= x]
+seqSort _ = []
+
+parSort2 :: (NFData a, Ord a) => Int -> [a] -> [a]
+parSort2 d list@(x:xs)
+  | d <= 0     = sort list
+  | otherwise = force greater `par` (force lesser `pseq`
+                                     (lesser ++ x:greater))
+      where lesser      = parSort2 d' [y | y <- xs, y <  x]
+            greater     = parSort2 d' [y | y <- xs, y >= x]
+            d' = d - 1
+parSort2 _ _              = []
+
---
+++ src/ch24/src/SortMain.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Data.Time.Clock (diffUTCTime, getCurrentTime)
+import System.Environment (getArgs)
+import System.Random (StdGen, getStdGen, randoms)
+import Control.DeepSeq (force)
+
+import QuickSort
+
+-- testFunction = sort
+-- testFunction = seqSort
+-- testFunction = parSort
+-- testFunction = parSort2 2
+
+randomInts :: Int -> StdGen -> [Int]
+randomInts k g = let result = take k (randoms g)
+                 in force result `seq` result
+
+mainTest testFunction = do
+  args <- getArgs
+  let count | null args = 500000
+            | otherwise = read (head args)
+  input <- randomInts count `fmap` getStdGen
+  putStrLn $ "We have " ++ show (length input) ++ " elements to sort."
+  start <- getCurrentTime
+  let sorted = testFunction input
+  putStrLn $ "Sorted all " ++ show (length sorted) ++ " elements."
+  end <- getCurrentTime
+  putStrLn $ show (end `diffUTCTime` start) ++ " elapsed."
+
+main = do
+  mainTest sort
+  mainTest seqSort
+  mainTest parSort
+  mainTest $ parSort2 2
