19df65b7f19fac2e6a4ad47e417d73c4a45de5a9
stites <sam@stites.io>
2015-09-21 19:36:03 -0700

    finish - example

--- src/ch24/src/LineChunks.hs
+++ src/ch24/src/LineChunks.hs
@@ -53,4 +53,27 @@ chunkedRead chunkFunc path = do
     chunk <- LB.take (chunkLength spec) `liftM` LB.hGetContents h
     return (chunk, h)
 
+-----------------------
 
+lineChunks :: Int -> FilePath -> IO [ChunkSpec]
+lineChunks numChunks path = do
+  bracket (openFile path ReadMode) hClose $ \h -> do
+    totalSize <- fromIntegral `liftM` hFileSize h
+    let chunkSize = totalSize `div` fromIntegral numChunks
+        findChunks offset = do
+          let newOffset = offset + chunkSize
+          hSeek h AbsoluteSeek (fromIntegral newOffset)
+          let findNewline off = do
+                eof <- hIsEOF h
+                if eof
+                  then return [CS offset (totalSize - offset)]
+                  else do
+                    bytes <- LB.hGet h 4096
+                    case LB.elemIndex '\n' bytes of
+                      Just n -> do
+                        chunks@(c:_) <- findChunks (off + n + 1)
+                        let coff = chunkOffset c
+                        return (CS offset (coff - offset):chunks)
+                      Nothing -> findNewline (off + LB.length bytes)
+          findNewline newOffset
+    findChunks 0
